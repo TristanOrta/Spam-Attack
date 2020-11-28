41e58c021101435e44e81f9df820df64da6566f9
oscar Tristan <oscar_tristan_13@hotmail.com>
2020-11-28 03:26:41 -0600

    first commit

---
+++ MailSpam/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace MailSpam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            string correoOrigen ;
+            string password;
+            string destino;
+            string asunto;
+            string mensaje;
+            int numero;
+
+            Console.WriteLine(" \n" +
+                "░██████╗██████╗░░█████╗░███╗░░░███╗  ░█████╗░████████╗████████╗░█████╗░░█████╗░██╗░░██╗ \n" +
+                "██╔════╝██╔══██╗██╔══██╗████╗░████║  ██╔══██╗╚══██╔══╝╚══██╔══╝██╔══██╗██╔══██╗██║░██╔╝ \n" +
+                "╚█████╗░██████╔╝███████║██╔████╔██║  ███████║░░░██║░░░░░░██║░░░███████║██║░░╚═╝█████═╝░ \n" +
+                "░╚═══██╗██╔═══╝░██╔══██║██║╚██╔╝██║  ██╔══██║░░░██║░░░░░░██║░░░██╔══██║██║░░██╗██╔═██╗░ \n" +
+                "██████╔╝██║░░░░░██║░░██║██║░╚═╝░██║  ██║░░██║░░░██║░░░░░░██║░░░██║░░██║╚█████╔╝██║░╚██╗ \n" +
+                "╚═════╝░╚═╝░░░░░╚═╝░░╚═╝╚═╝░░░░░╚═╝  ╚═╝░░╚═╝░░░╚═╝░░░░░░╚═╝░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝ \n" +
+                 "from: \n" +
+                " -https://github.com/TristanOrta- \n" +
+                "\n" +
+                "version beta : (solo funciona con correos Gmail leer documentacion del proyecto\n" +
+                "\n" +
+                "\n" );
+
+
+            Console.WriteLine("Introduce tu correo");
+            correoOrigen = Console.ReadLine();
+
+            Console.WriteLine("Introduce tu contraseña");
+            password = Console.ReadLine();
+
+            Console.WriteLine("Introduce el destinatario");
+            destino = Console.ReadLine();
+
+            Console.WriteLine("Introduce el asunto del correo");
+            asunto = Console.ReadLine();
+
+            Console.WriteLine("Introduce el mensaje del correo");
+            mensaje = Console.ReadLine();
+
+            Console.WriteLine("Introduce el numeo de veces a mandar el correo: ");
+            numero = Int32.Parse(Console.ReadLine());
+
+            string validaGmail = correoOrigen.Substring(correoOrigen.Length - 10);
+
+            if (validaGmail == "@gmail.com")
+            {
+                servicorDeCorreo.Correogmail(correoOrigen, destino, asunto, mensaje, password,numero);
+            }
+            else
+            {
+                Console.WriteLine("Error utiliza un dominio de correo valido ejemplo:  miCorreo@gmail");
+            }
+
+
+        }
+    }
+}
---
+++ MailSpam/servicorDeCorreo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net.Mail;
+
+namespace MailSpam
+{
+    class servicorDeCorreo
+    {
+        public static void Correogmail(string correoOrigen,string destino,string asunto,string mensaje,string password, int numero)
+        {
+            string errorMessaje = "ERROR: revisa que tu correo y/o contraseña esten correctos";
+            string exito = "Correo enviado con exito";
+            for (int i = 1; i <= numero; i++)
+            {
+                try
+                {
+                    
+                    MailMessage oMailMessage = new MailMessage(correoOrigen, destino, asunto, mensaje);
+
+                    SmtpClient smtpClient = new SmtpClient("smtp.gmail.com");
+                    smtpClient.EnableSsl = true;
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Host = "smtp.gmail.com";
+                    smtpClient.Port = 587;
+                    smtpClient.Credentials = new System.Net.NetworkCredential(correoOrigen, password);
+
+                    smtpClient.Send(oMailMessage);
+
+                    Console.WriteLine(exito);
+
+                    smtpClient.Dispose();
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(errorMessaje + ex);
+                }
+            }
+
+
+         
+        }
+
+    }
+}
